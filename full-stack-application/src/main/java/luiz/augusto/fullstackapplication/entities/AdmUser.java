c2f1901b1b6bf4c8e4e5270c901a05994b372c85
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-09-13 16:24:53 -0300

    AdmUser entity

---
+++ full-stack-application/src/main/java/luiz/augusto/fullstackapplication/entities/AdmUser.java
@@ -0,0 +1,24 @@
+package luiz.augusto.fullstackapplication.entities;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "tb_adm_user")
+@PrimaryKeyJoinColumn(name = "user_id", referencedColumnName = "id")
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+public class AdmUser extends User{
+
+    private String username;
+
+    public AdmUser(String email, String password) {
+        super(email, password, "ADMIN");
+    }
+}
