3256f8cc8ba6a684a978850ccb6db32ebcd709a7
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-09-13 16:35:18 -0300

    adding spring security

--- full-stack-application/src/main/java/luiz/augusto/fullstackapplication/controllers/UserController.java
+++ full-stack-application/src/main/java/luiz/augusto/fullstackapplication/controllers/UserController.java
@@ -1,6 +1,10 @@
 package luiz.augusto.fullstackapplication.controllers;
 
+import luiz.augusto.fullstackapplication.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
 
+    @GetMapping("/test")
+    public ResponseEntity<String> test()
+    {
+        return ResponseEntity.ok("successful!");
+    }
 }
